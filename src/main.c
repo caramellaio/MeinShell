68fff247f96579eadfbc07916f8cae457b5ffacb
caramellaio <albertobombardelli123@gmail.com>
2018-05-21 21:37:04 +0200

    main cleanup

--- src/main.c
+++ src/main.c
@@ -11,14 +11,6 @@ int main(int argc, char **argv) {
       return 1;
   }
 
-  char *ls[] = {"ls", NULL};
-  char *log[] = {"log", NULL};
-  char *log2[] = {"log", NULL};
-  char *log3[] = {"log", NULL};
-  char *grep[] = {"grep", "bin", NULL};
-  char *wc[] = {"wc", NULL};
-  char **cmd[] = {ls, log, grep, log2, wc, log3, NULL};
-  loop_pipe(cmd, 1, "redir.txt");
   Shell * shell = Shell_init();
   Shell_configure(shell, argc, argv);
   Shell_start(shell);

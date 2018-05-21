52bc5bb59252e0db625d177741edd15272c6c2f8
caramellaio <albertobombardelli123@gmail.com>
2018-05-21 23:51:08 +0200

    update get_logger_call_string
    still work in progress.

--- src/shell/config.c
+++ src/shell/config.c
@@ -1,6 +1,7 @@
 #include "config.h"
 
 static int get_str_size(char *string);
+static void string_array_to_string(char **array, char **out);
 
 ShellConfig *ShellConfig_init() {
   ShellConfig *retval;
@@ -47,51 +48,40 @@ void ShellConfig_set_output_max_size(ShellConfig *self, int out_max_size) {
   self->output_max_size = out_max_size;
 }
 
-char *ShellConfig_get_logger_call_string(ShellConfig *self, int is_err) {
-  char *logger_str;
-  char max_size_str[256];
-
-  int logger_p_size;
-  int out_file_size; 
-  int err_file_size;
-  int max_size_size;
-
-  int logger_str_size; 
-
-  logger_p_size = get_str_size(self->logger_path);
-  if (is_err) {
-    out_file_size = get_str_size(self->err_file);
-  }
-  else {
-    out_file_size = get_str_size(self->out_file);
-  }
-
-  sprintf(max_size_str, "%d\0", self->output_max_size);
-
-  max_size_size = get_str_size(max_size_str);
-
-  logger_str_size = logger_p_size + out_file_size + max_size_size + 12;
-
-  if (self->print_code)
-    logger_str_size += 4;
-
-  logger_str = (char *)malloc(sizeof(char)*logger_str_size+1);
-
-  strcpy(self->logger_path, logger_str);
-  strcat(logger_str, " -o ");
-  strcat(logger_str, self->out_file);
-  strcat(logger_str, " -m ");
-  strcat(logger_str, max_size_str);
+char *ShellConfig_get_logger_call_string(ShellConfig *self, int is_err, int pid,
+                                         int code, char * input, char **command) {
+  char buffer[512];
+  char command_str[512];
+  char *retval;
+  int retval_size;
 
   if (self->print_code) {
-    strcat(logger_str, " -p ");
+    sprintf(buffer, "%s -o %s -m %d -n %s -s %s -p %d -C -c %d\0", 
+            self->logger_path, is_err ? self->err_file : self->out_file,
+            self->output_max_size, input, command_str, pid, code);
+  }
+  else {
+    sprintf(buffer, "%s -o %s -m %d -n %s -s %s -p %d\0", 
+            self->logger_path, is_err ? self->err_file : self->out_file,
+            self->output_max_size, input, command_str, pid);
   }
 
-  logger_str[logger_str_size] = '\0';
+  retval_size = get_str_size(buffer);
 
-  return logger_str;
+  retval = (char *)malloc(sizeof(char)*retval_size+1);
+
+  strncpy(retval, buffer, retval_size);
+  retval[retval_size] = '\0';
+  
+  return retval;
 }
 
+static void string_array_to_string(char **array, char **out) {
+  while (*array != NULL) {
+    strcat(*out, *array);
+    array++;
+  }
+}
 static int get_str_size(char *string) {
   int i;
   for (i = 0; string[i] != EOF && string[i] != '\0' && string[i] != NULL; i++);
--- src/shell/config.h
+++ src/shell/config.h
@@ -33,5 +33,6 @@ void ShellConfig_enable_print_code(ShellConfig *self);
 
 void ShellConfig_set_output_max_size(ShellConfig *self, int out_max_size);
 
-char *ShellConfig_get_logger_call_string(ShellConfig *self, int is_err);
+char *ShellConfig_get_logger_call_string(ShellConfig *self, int is_err, int pid,
+                                         int code, char * input, char **command);
 #endif
